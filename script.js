e1ec7d92ea037e779f84ea6f8cb53766326bf800
saikatali1975 <74508916+saikatali1975@users.noreply.github.com>
2020-12-12 12:11:59 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-  var x = document.getElementById("nav-id");
-  if (x.className === "nav-header") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-header";
-  }
-}
